1a2811fd72d05b3200153ebea399d932448446bf
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-01-14 21:57:06 -0500

    implement flatten

--- hw1.rkt
+++ hw1.rkt
@@ -50,10 +50,74 @@
      (error 'uniquify-expr "unsupported form: ~s~n" unsupported)]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Flatten
+
+(define (flatten pgm)
+  (match pgm
+    [`(program ,e)
+     (let-values ([(_ pgm e) (flatten-expr '() '() e)])
+       ; (printf "collect-binds result: ~s~n" (collect-binds pgm))
+       `(program ,(collect-binds pgm) ,(reverse (cons `(return ,e) pgm))))]
+
+    [_ (error 'flatten "Expected a (program ...) form, found ~s~n" pgm)]))
+
+(define (collect-binds pgm)
+  ; (printf "collect-binds: ~s~n" pgm)
+  (match pgm
+    [(list) '()]
+    [(cons `(assign ,x ,_) t)
+     (cons x (collect-binds t))]
+    [_ (error 'collect-binds "unsupported form: ~s~n" pgm)]))
+
+(define (arg? e) (or (fixnum? e) (symbol? e)))
+
+; flatten-expr : [(var, var)] -> [stmt] -> expr -> arg
+(define (flatten-expr binds pgm expr)
+  (match expr
+
+    [(? fixnum?)
+     (values binds pgm expr)]
+
+    [`(read)
+     (let [(fresh (gensym "tmp"))]
+       (values binds (cons `(assign ,fresh (read)) pgm) fresh))]
+
+    [`(- ,e1)
+     (let-values ([(binds pgm e1) (flatten-expr binds pgm e1)])
+       (let [(fresh (gensym "tmp"))]
+         (values binds (cons `(assign ,fresh (- ,e1)) pgm) fresh)))]
+
+    [`(+ ,e1 ,e2)
+     (let-values ([(binds pgm e1) (flatten-expr binds pgm e1)])
+       (let-values ([(binds pgm e2) (flatten-expr binds pgm e2)])
+         (let [(fresh (gensym "tmp"))]
+           (values binds (cons `(assign ,fresh (+ ,e1 ,e2)) pgm) fresh))))]
+
+    [(? symbol?)
+     (values binds pgm (car (lookup expr binds)))]
+
+    [`(let ([,var ,e1]) ,body)
+     (let-values ([(binds pgm e1) (flatten-expr binds pgm e1)])
+       (let [(fresh (gensym "tmp"))]
+         (let-values ([(binds pgm body)
+                       (flatten-expr (cons `(,var ,fresh) binds)
+                                     (cons `(assign ,fresh ,e1) pgm)
+                                     body)])
+           (values binds pgm body))))]
+
+    [_ (error 'flatten-expr "unsupported form: ~s~n" expr)]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tests
 
-(interp-tests "uniquify"
-              `(("uniquify" ,uniquify ,interp-scheme))
-              interp-scheme
-              "uniquify"
-              (range 1 6))
+; (interp-tests "uniquify"
+;               `(("uniquify" ,uniquify ,interp-scheme))
+;               interp-scheme
+;               "uniquify"
+;               (range 1 6))
+
+(flatten '(program (+ 1 2)))
+(flatten '(program (let [(x 10)] x)))
+(flatten '(program (let [(x 10)]
+                     (let [(y 20)]
+                       (+ (- y) (+ x y))))))
