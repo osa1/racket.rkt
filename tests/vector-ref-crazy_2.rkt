b7b3120b9a7027f0b2c683bfa4a31d93f2c9f7a9
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-02-16 16:21:27 -0500

    formatting

--- tests/vector-ref-crazy_2.rkt
+++ tests/vector-ref-crazy_2.rkt
@@ -1,1 +1,6 @@
-(if (let ((vector-2 (vector #t))) (let ((vector-1 (vector #t))) (and (vector-ref vector-2 0) (and (vector-ref vector-1 0) #t)))) 42 123)
+(if (let ((vector-2 (vector #t)))
+      (let ((vector-1 (vector #t)))
+        (and (vector-ref vector-2 0)
+             (and (vector-ref vector-1 0)
+                  #t))))
+  42 123)
