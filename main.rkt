4cb0245c4922f34667def8767ab3f89e99dfb8bb
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-02-15 19:15:23 -0500

    main: only run gcc when file is printed

--- main.rkt
+++ main.rkt
@@ -9,9 +9,12 @@
     (unless (file-exists? file)
       (error 'main "File does not exist: ~a" file)))
 
+  (debug-level 4)
+
   (for ([file files])
     (let ([path (string->path file)])
       (let-values ([(_1 name _2) (split-path path)])
 	(let ([name (car (string-split (path->string name) "."))])
-	  ((compile-file typechecker r3-passes) file)
-	  (system (format "gcc -g runtime.o tests/~a.s -o ~a" name name)))))))
+	  (if ((compile-file typechecker r3-passes) file)
+	    (system (format "gcc -g runtime.o tests/~a.s -o ~a" name name))
+	    (printf "Compilation failed: ~a~n" file)))))))
