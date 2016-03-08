4288a8cf3dc9c705280d83cbc6f27ecc574aa5c0
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-03-07 19:39:11 -0500

    spill tests. disable interpreter tests.

--- test.rkt
+++ test.rkt
@@ -16,49 +16,23 @@
          (start-from step (cdr passes))]))
 
 (define (run-all-tests)
-  ; (interp-tests "first assignment" typecheck r1-passes interp-scheme "uniquify" (range 1 6))
+  (compiler-tests "spill" typechecker r1-passes "spill" (range 1 16))
   (compiler-tests "first assignment" typechecker r1-passes "uniquify" (range 1 6))
-
-  ; (interp-tests "first assignment" typecheck r1-passes interp-scheme "flatten" (range 1 5))
   (compiler-tests "first assignment" typechecker r1-passes "flatten" (range 1 5))
-
-  ; (interp-tests "r0" typecheck r1-passes interp-scheme "r0" (range 1 5))
   (compiler-tests "r0" typechecker r1-passes "r0" (range 1 5))
-
-  ; (interp-tests "r1" typecheck r1-passes interp-scheme "r1" (range 1 22))
   (compiler-tests "r1" typechecker r1-passes "r1" (range 1 22))
-
-  ; This fails because apparently x86 interpreter doesn't support pushq and popq
-  ; (interp-tests "r1a" typecheck r1-passes interp-scheme "r1a" (range 1 9))
   (compiler-tests "r1a" typechecker r1-passes "r1a" (range 1 9))
-
-
-  ; (interp-tests "forum" typecheck r1-passes interp-scheme "forum" (range 1 2))
   (compiler-tests "forum" typechecker r1-passes "forum" (range 1 2))
-
-
-  ; Similarly fails because of pushq/popq
-  ; (interp-tests "crazy" typecheck r1-passes interp-scheme "crazy" (range 1 3))
   (compiler-tests "crazy" typechecker r1-passes "crazy" (range 1 3))
-
-  ; (interp-tests "ty" typechecker r1-passes interp-scheme "ty" (range 1 11))
   (compiler-tests "ty" typechecker r1-passes "ty" (range 1 12))
-
-  ; (interp-tests "conditionals-1" typecheck r2-passes interp-scheme "cond" (range 1 5))
   (compiler-tests "conditionals-1" typechecker r1-passes "cond" (range 1 5))
-
-
-  ; (interp-tests "conditionals-2" typecheck r1-passes interp-scheme "ty" (range 1 6))
   (compiler-tests "conditionals-2" typechecker r1-passes "ty" (range 1 6))
-
-  ; (interp-tests "r2" typecheck r2-passes interp-scheme "r2" (range 1 24))
   (compiler-tests "r2" typechecker r2-passes "r2" (range 1 24))
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Vectors
 
   ; (debug-level 4)
-  ; (interp-tests "vec" typecheck r3-passes interp-scheme "vector" (range 1 10))
   (compiler-tests "vec" typechecker r2-passes "vector" (range 1 10))
 
   ; It takes forever after vector-ref-crazy_20
