ced4b4c6af440656b18627f76860d98a32e075a8
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-03-09 19:08:26 -0500

    a simple trick for slightly better asm generation when no rsp alignment is needed

--- passes/reg-alloc/assign-homes.rkt
+++ passes/reg-alloc/assign-homes.rkt
@@ -15,15 +15,37 @@
   (match def
     [`(define ,tag : ,ret-ty ,stack-locs-used . ,instrs)
      `(define ,tag : ,ret-ty
-        (,(align-stack (* 8 stack-locs-used)))
+        (,(align-stack stack-locs-used instrs))
         ,@(assign-home-instrs mapping instrs))]
     [_ (unsupported-form 'assign-homes def)]))
 
-(define (align-stack stack)
-  ; TODO: Document this. Since we don't push %rbp anymore we need to play this
-  ; game.
-  ; TODO: Only do this if the function calls functions.
-  (+ stack 8))
+(define (align-stack stack-locs-used instrs)
+
+  ; According to the ABI, on function entry %rsp+8 should be 16-byte aligned.
+  ; We know initially this is the case (because the caller makes sure) but here
+  ; we need to manually align %rsp if we call other functions.
+  ;
+  ; In the 32-bit conventions we'd do something like this:
+  ;
+  ;   pushq %ebp
+  ;   movq  %rsp, %ebp
+  ;
+  ; This makes %rsp 16-byte aligned. Now, in x86_64 we don't do this, so the
+  ; alignment code is a bit different. If in the stack frame we have even
+  ; number of things (assuming every "thing" is 8-byte), we need to increment
+  ; stack pointer 8-byte more, so that when we call a function, caller find
+  ; %rsp+8 16-byte aligned.
+
+  (if (even? stack-locs-used)
+
+    ; Here's one little optimization-like thing that we could do sometimes. If
+    ; we don't call any functions in the function then we don't need to align
+    ; anything. Sometimes saves us two instructions if the function doesn't
+    ; need any stack space.
+    (if (has-callq? instrs)
+      (* 8 (+ 1 stack-locs-used))
+      (* 8 stack-locs-used))
+    (* 8 stack-locs-used)))
 
 (define (assign-home-instrs asgns instrs)
   (map (lambda (instr) (assign-home-instr asgns instr)) instrs))
@@ -71,3 +93,27 @@
              [#t (error 'assign-home-arg "Variable mapped to an unknown thing: ~s~n"
                         asgn)]))]
     [_ (unsupported-form 'assign-home-arg arg)]))
+
+(define (has-callq? instrs)
+  (match instrs
+    [`() #f]
+    [`(,instr . ,instrs)
+     (match instr
+       [`(if ,_ ,pgm-t ,pgm-f)
+        (or (has-callq? pgm-t) (has-callq? pgm-f) (has-callq? instrs))]
+
+       [`(,(or 'addq 'subq 'movq 'leaq 'cmpq 'xorq) ,_ ,_)
+        (has-callq? instrs)]
+
+       [`(,(or 'negq 'pushq 'popq) ,arg)
+        (has-callq? instrs)]
+
+       [`(callq ,_) #t]
+
+       [`(retq) (has-callq? instrs)]
+
+       [`(,(or 'sete 'setl) (byte-reg al)) (has-callq? instrs)]
+
+       [`(movzbq (byte-reg al) ,_) (has-callq? instrs)]
+
+       [_ (unsupported-form 'assign-home-instr instr)])]))
