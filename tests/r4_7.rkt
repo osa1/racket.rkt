f2e180b16e96ba93e263acaf6841daff8bb6e15b
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-04-29 11:33:41 -0400

    minor: format some tests

--- tests/r4_7.rkt
+++ tests/r4_7.rkt
@@ -1,9 +1,11 @@
-(define (even? [x : Integer]) : Boolean 
+(define (even? [x : Integer]) : Boolean
   (if (eq? x 0)
-      #t
-      (odd? (+ (- 1) x))))
-(define (odd? [x : Integer]) : Boolean 
+    #t
+    (odd? (+ (- 1) x))))
+
+(define (odd? [x : Integer]) : Boolean
   (if (eq? x 0)
-      #f
-      (even? (+ (- 1) x))))
+    #f
+    (even? (+ (- 1) x))))
+
 (if (even? (read)) 999 42)
